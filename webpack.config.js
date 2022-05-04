deb29cb31b06e47a41cb056a81e796cd4ee10385
KizievDmitriy <kizievdmitrii@gmail.com>
2022-05-04 13:01:20 +0300

    add scripts json

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development', // mode: 'production',
-    entry: './src/index.js', // точка входа
+    mode: "development", // mode: 'production',
+    entry: "./src/index.js", // точка входа
     output: {   // точка выхода
-    path: path.resolve(__dirname, 'build'),    
-    filename: 'main.js',     //название выходного файла
+    path: path.resolve(__dirname, "build"),    
+    filename: "main.js",     //название выходного файла
   },
 }
