c2deb59acfbb6e7808cc09431453764cac95f9d1
KizievDmitriy <kizievdmitrii@gmail.com>
2022-05-04 12:10:37 +0300

    add entry and output
    Сделал точку вход и точку выхода

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.js', // точка входа
+    output: {   // точка выхода
+    path: path.resolve(__dirname, 'build'),    
+    filename: 'main.js',     //название выходного файла
+  },
 }
